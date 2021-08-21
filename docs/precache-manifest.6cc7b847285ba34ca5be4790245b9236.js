53e8588b0159343994bb75f44be4c9cf3db416ab
Steven Cabrera Londoño <32532691+stevenstc@users.noreply.github.com>
2021-08-21 12:05:48 -0500

    Update sendblue

---
+++ docs/precache-manifest.6cc7b847285ba34ca5be4790245b9236.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c1e2c99d8cc4a9542e8ae6e492f1845",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f055b136a8296688960",
+    "url": "/static/js/2.48808265.chunk.js"
+  },
+  {
+    "revision": "d5b445a81227876e9f4080bf0648eb92",
+    "url": "/static/js/2.48808265.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46953cb9c5eb61d6b24d",
+    "url": "/static/js/main.f0fb99cf.chunk.js"
+  },
+  {
+    "revision": "19af52671b05553f8765",
+    "url": "/static/js/runtime-main.47b912c7.js"
+  }
+]);
