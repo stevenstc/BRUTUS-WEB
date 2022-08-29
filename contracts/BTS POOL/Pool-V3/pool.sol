1fdda12c0a30c07c188ce5217c7e8fa8595976a2
Steven Cabrera Londoño <32532691+stevenstc@users.noreply.github.com>
2022-08-29 08:06:45 -0500

    Update pool.sol

--- contracts/BTS POOL/Pool-V3/pool.sol
+++ contracts/BTS POOL/Pool-V3/pool.sol
@@ -6,23 +6,16 @@ import "./SafeMath.sol";
 
 interface TRC20_Interface {
 
-    function allowance(address _owner, address _spender) external view returns (uint remaining);
+  function allowance(address _owner, address _spender) external view returns (uint remaining);
+  function transferFrom(address _from, address _to, uint _value) external returns (bool);
+  function transfer(address direccion, uint cantidad) external returns (bool);
+  function balanceOf(address who) external view returns (uint256);
+  function decimals() external view returns (uint256);
+  function totalSupply() external view returns (uint256);
+  function issue(uint amount) external;
+  function redeem(uint amount) external;
+  function transferOwnership(address newOwner) external;
 
-    function transferFrom(address _from, address _to, uint _value) external returns (bool);
-
-    function transfer(address direccion, uint cantidad) external returns (bool);
-
-    function balanceOf(address who) external view returns (uint256);
-
-    function decimals() external view returns (uint256);
-
-    function totalSupply() external view returns (uint256);
-
-    function issue(uint amount) external;
-        
-    function redeem(uint amount) external;
-
-    function transferOwnership(address newOwner) external;
 }
 
 contract PoolBRSTv3 is Ownable{
